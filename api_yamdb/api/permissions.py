63e813894bd6c78dc07f350c5954c6375f0c4d50
Виктория Карина <victoria.m.karina@mail.ru>
2023-03-28 21:36:43 +0300

    Добавила User

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,32 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class IsAdminUser(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and (
+            request.user.is_admin
+            or request.user.is_staff
+            or request.user.is_superuser
+        )
+
+
+class IsAdminUserOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS or (
+            request.user.is_authenticated and request.user.is_admin)
+
+
+class IsAdminModeratorAuthorOrReadOnly(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        if request.method == 'POST':
+            return request.user.is_authenticated
+        return (request.user.is_authenticated and (
+            request.user == obj.author
+            or request.user.is_admin
+            or request.user.moderator
+        ))
